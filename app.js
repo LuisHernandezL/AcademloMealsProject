f6659941b700b67828db16ef2c2dcf944e4d983f
anodrac99 <juancho-cm2000@hotmail.com>
2022-07-07 15:22:36 -0500

    test

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+const { restaurantRouter } = require('./routes/restaurants.routes')
 
 module.exports = { app };
