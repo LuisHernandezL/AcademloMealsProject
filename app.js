0f0af66d686242c93510fbf6caa35a3dff2b984e
LuisHernandezL <luishernandz2503@gmail.com>
2022-07-07 16:21:14 -0400

    agregando app error a appjs prueba

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 //init app
 const app = express();
 
+//import app error
+const { AppError } = './utils/appError.utils.js';
+
 app.use(express.json());
 
 module.exports = { app };
